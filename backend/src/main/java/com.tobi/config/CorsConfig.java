a981ab1fed6d592e0f188551adfeeda55eb2a420
igor21211 <spuraigor@gmail.com>
2023-08-14 22:22:57 +0300

    add Actuator

--- backend/src/main/java/com.tobi/config/CorsConfig.java
+++ backend/src/main/java/com.tobi/config/CorsConfig.java
@@ -32,6 +32,7 @@ public class CorsConfig {
         configuration.setAllowedMethods(allowedMethods);
         configuration.setAllowedHeaders(allowedHeaders);
         configuration.setExposedHeaders(expectedHeaders);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/api/**", configuration);
         return source;
