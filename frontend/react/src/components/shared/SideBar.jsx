6fcab2eed272feebebea96b6c35f9adab0a24526
igor21211 <spuraigor@gmail.com>
2023-08-14 22:31:29 +0300

    add red

--- frontend/react/src/components/shared/SideBar.jsx
+++ frontend/react/src/components/shared/SideBar.jsx
@@ -1,222 +1,244 @@
 'use client'
 
 import {
-    IconButton,
-    Avatar,
-    Box,
-    CloseButton,
-    Flex,
-    HStack,
-    VStack,
-    Icon,
-    useColorModeValue,
-    Text,
-    Drawer,
-    DrawerContent,
-    useDisclosure,
-    Menu,
-    MenuButton,
-    MenuDivider,
-    MenuItem,
-    MenuList,
-    Image
+	Avatar,
+	Box,
+	CloseButton,
+	Drawer,
+	DrawerContent,
+	Flex,
+	HStack,
+	Icon,
+	IconButton,
+	Image,
+	Menu,
+	MenuButton,
+	MenuDivider,
+	MenuItem,
+	MenuList,
+	Text,
+	VStack,
+	useColorModeValue,
+	useDisclosure,
 } from '@chakra-ui/react'
 import {
-    FiHome,
-    FiTrendingUp,
-    FiCompass,
-    FiStar,
-    FiSettings,
-    FiMenu,
-    FiBell,
-    FiChevronDown,
+	FiBell,
+	FiChevronDown,
+	FiCompass,
+	FiHome,
+	FiMenu,
+	FiSettings,
+	FiStar,
+	FiTrendingUp,
 } from 'react-icons/fi'
-import {useAuth} from "../context/AuthContext.jsx";
-
-
-
-
+import { useAuth } from '../context/AuthContext.jsx'
 
 const LinkItems = [
-    { name: 'Home', icon: FiHome },
-    { name: 'Trending', icon: FiTrendingUp },
-    { name: 'Explore', icon: FiCompass },
-    { name: 'Favourites', icon: FiStar },
-    { name: 'Settings', icon: FiSettings },
+	{ name: 'Home', icon: FiHome },
+	{ name: 'Trending', icon: FiTrendingUp },
+	{ name: 'Explore', icon: FiCompass },
+	{ name: 'Favourites', icon: FiStar },
+	{ name: 'Settings', icon: FiSettings },
 ]
 
 const SidebarContent = ({ onClose, ...rest }) => {
-    return (
-        <Box
-            transition="3s ease"
-            bg={useColorModeValue('white', 'gray.900')}
-            borderRight="1px"
-            borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-            w={{ base: 'full', md: 60 }}
-            pos="fixed"
-            h="full"
-            {...rest}>
-            <Flex h="20" flexDirection={"column"} alignItems="center" mx="8" mb={75} mt={7} justifyContent="space-between">
-                <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold" mb={5}>
-                    Dashboard
-                </Text>
-                <Image
-                    borderRadius='full'
-                    boxSize='75px'
-                    src='https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/ap7mhfgvjzfy1v6jyn7n'
-                    alt='App-React'
-                />
-                <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
-            </Flex>
-            {LinkItems.map((link) => (
-                <NavItem key={link.name} icon={link.icon}>
-                    {link.name}
-                </NavItem>
-            ))}
-        </Box>
-    )
+	return (
+		<Box
+			transition='3s ease'
+			bg={useColorModeValue('white', 'gray.900')}
+			borderRight='1px'
+			borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+			w={{ base: 'full', md: 60 }}
+			pos='fixed'
+			h='full'
+			{...rest}
+		>
+			<Flex
+				h='20'
+				flexDirection={'column'}
+				alignItems='center'
+				mx='8'
+				mb={75}
+				mt={7}
+				justifyContent='space-between'
+			>
+				<Text fontSize='2xl' fontFamily='monospace' fontWeight='bold' mb={5}>
+					Dashboard
+				</Text>
+				<Image
+					borderRadius='full'
+					boxSize='75px'
+					src='https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/ap7mhfgvjzfy1v6jyn7n'
+					alt='App-React'
+				/>
+				<CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+			</Flex>
+			{LinkItems.map(link => (
+				<NavItem key={link.name} icon={link.icon}>
+					{link.name}
+				</NavItem>
+			))}
+		</Box>
+	)
 }
 
 const NavItem = ({ icon, children, ...rest }) => {
-    return (
-        <Box
-            as="a"
-            href="#"
-            style={{ textDecoration: 'none' }}
-            _focus={{ boxShadow: 'none' }}>
-            <Flex
-                align="center"
-                p="4"
-                mx="4"
-                borderRadius="lg"
-                role="group"
-                cursor="pointer"
-                _hover={{
-                    bg: 'blue.400',
-                    color: 'white',
-                }}
-                {...rest}>
-                {icon && (
-                    <Icon
-                        mr="4"
-                        fontSize="16"
-                        _groupHover={{
-                            color: 'white',
-                        }}
-                        as={icon}
-                    />
-                )}
-                {children}
-            </Flex>
-        </Box>
-    )
+	return (
+		<Box
+			as='a'
+			href='#'
+			style={{ textDecoration: 'none' }}
+			_focus={{ boxShadow: 'none' }}
+		>
+			<Flex
+				align='center'
+				p='4'
+				mx='4'
+				borderRadius='lg'
+				role='group'
+				cursor='pointer'
+				_hover={{
+					bg: 'red.400',
+					color: 'white',
+				}}
+				{...rest}
+			>
+				{icon && (
+					<Icon
+						mr='4'
+						fontSize='16'
+						_groupHover={{
+							color: 'white',
+						}}
+						as={icon}
+					/>
+				)}
+				{children}
+			</Flex>
+		</Box>
+	)
 }
 
 const MobileNav = ({ onOpen, ...rest }) => {
-    const { logOut, customer } = useAuth();
-    return (
-        <Flex
-            ml={{ base: 0, md: 60 }}
-            px={{ base: 4, md: 4 }}
-            height="20"
-            alignItems="center"
-            bg={useColorModeValue('white', 'gray.900')}
-            borderBottomWidth="1px"
-            borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-            justifyContent={{ base: 'space-between', md: 'flex-end' }}
-            {...rest}>
-            <IconButton
-                display={{ base: 'flex', md: 'none' }}
-                onClick={onOpen}
-                variant="outline"
-                aria-label="open menu"
-                icon={<FiMenu />}
-            />
+	const { logOut, customer } = useAuth()
+	return (
+		<Flex
+			ml={{ base: 0, md: 60 }}
+			px={{ base: 4, md: 4 }}
+			height='20'
+			alignItems='center'
+			bg={useColorModeValue('white', 'gray.900')}
+			borderBottomWidth='1px'
+			borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+			justifyContent={{ base: 'space-between', md: 'flex-end' }}
+			{...rest}
+		>
+			<IconButton
+				display={{ base: 'flex', md: 'none' }}
+				onClick={onOpen}
+				variant='outline'
+				aria-label='open menu'
+				icon={<FiMenu />}
+			/>
 
-            <Text
-                display={{ base: 'flex', md: 'none' }}
-                fontSize="2xl"
-                fontFamily="monospace"
-                fontWeight="bold">
-                <Image
-                    borderRadius='full'
-                    boxSize='75px'
-                    src='https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/ap7mhfgvjzfy1v6jyn7n'
-                    alt='App-React'
-                />
-            </Text>
+			<Text
+				display={{ base: 'flex', md: 'none' }}
+				fontSize='2xl'
+				fontFamily='monospace'
+				fontWeight='bold'
+			>
+				<Image
+					borderRadius='full'
+					boxSize='75px'
+					src='https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/ap7mhfgvjzfy1v6jyn7n'
+					alt='App-React'
+				/>
+			</Text>
 
-            <HStack spacing={{ base: '0', md: '6' }}>
-                <IconButton size="lg" variant="ghost" aria-label="open menu" icon={<FiBell />} />
-                <Flex alignItems={'center'}>
-                    <Menu>
-                        <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
-                            <HStack>
-                                <Avatar
-                                    size={'sm'}
-                                    src={
-                                        'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                                    }
-                                />
-                                <VStack
-                                    display={{ base: 'none', md: 'flex' }}
-                                    alignItems="flex-start"
-                                    spacing="1px"
-                                    ml="2">
-                                    <Text fontSize="sm">{customer?.username}</Text>
-                                    {customer?.roles.map((role, id)=>(
-                                        <Text key={id} fontSize="xs" color="gray.600">
-                                            {role}
-                                        </Text>
-                                    ))}
-
-                                </VStack>
-                                <Box display={{ base: 'none', md: 'flex' }}>
-                                    <FiChevronDown />
-                                </Box>
-                            </HStack>
-                        </MenuButton>
-                        <MenuList
-                            bg={useColorModeValue('white', 'gray.900')}
-                            borderColor={useColorModeValue('gray.200', 'gray.700')}>
-                            <MenuItem>Profile</MenuItem>
-                            <MenuItem>Settings</MenuItem>
-                            <MenuItem>Billing</MenuItem>
-                            <MenuDivider />
-                            <MenuItem onClick={logOut}>
-                                Sign out</MenuItem>
-                        </MenuList>
-                    </Menu>
-                </Flex>
-            </HStack>
-        </Flex>
-    )
+			<HStack spacing={{ base: '0', md: '6' }}>
+				<IconButton
+					size='lg'
+					variant='ghost'
+					aria-label='open menu'
+					icon={<FiBell />}
+				/>
+				<Flex alignItems={'center'}>
+					<Menu>
+						<MenuButton
+							py={2}
+							transition='all 0.3s'
+							_focus={{ boxShadow: 'none' }}
+						>
+							<HStack>
+								<Avatar
+									size={'sm'}
+									src={
+										'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+									}
+								/>
+								<VStack
+									display={{ base: 'none', md: 'flex' }}
+									alignItems='flex-start'
+									spacing='1px'
+									ml='2'
+								>
+									<Text fontSize='sm'>{customer?.username}</Text>
+									{customer?.roles.map((role, id) => (
+										<Text key={id} fontSize='xs' color='gray.600'>
+											{role}
+										</Text>
+									))}
+								</VStack>
+								<Box display={{ base: 'none', md: 'flex' }}>
+									<FiChevronDown />
+								</Box>
+							</HStack>
+						</MenuButton>
+						<MenuList
+							bg={useColorModeValue('white', 'gray.900')}
+							borderColor={useColorModeValue('gray.200', 'gray.700')}
+						>
+							<MenuItem>Profile</MenuItem>
+							<MenuItem>Settings</MenuItem>
+							<MenuItem>Billing</MenuItem>
+							<MenuDivider />
+							<MenuItem onClick={logOut}>Sign out</MenuItem>
+						</MenuList>
+					</Menu>
+				</Flex>
+			</HStack>
+		</Flex>
+	)
 }
 
-const SidebarWithHeader = ({children}) => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+const SidebarWithHeader = ({ children }) => {
+	const { isOpen, onOpen, onClose } = useDisclosure()
 
-    return (
-        <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-            <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
-            <Drawer
-                isOpen={isOpen}
-                placement="left"
-                onClose={onClose}
-                returnFocusOnClose={false}
-                onOverlayClick={onClose}
-                size="full">
-                <DrawerContent>
-                    <SidebarContent onClose={onClose} />
-                </DrawerContent>
-            </Drawer>
-            {/* mobilenav */}
-            <MobileNav onOpen={onOpen} />
-            <Box ml={{ base: 0, md: 60 }} p="4">
-                {children}
-            </Box>
-        </Box>
-    )
+	return (
+		<Box minH='100vh' bg={useColorModeValue('gray.100', 'gray.900')}>
+			<SidebarContent
+				onClose={() => onClose}
+				display={{ base: 'none', md: 'block' }}
+			/>
+			<Drawer
+				isOpen={isOpen}
+				placement='left'
+				onClose={onClose}
+				returnFocusOnClose={false}
+				onOverlayClick={onClose}
+				size='full'
+			>
+				<DrawerContent>
+					<SidebarContent onClose={onClose} />
+				</DrawerContent>
+			</Drawer>
+			{/* mobilenav */}
+			<MobileNav onOpen={onOpen} />
+			<Box ml={{ base: 0, md: 60 }} p='4'>
+				{children}
+			</Box>
+		</Box>
+	)
 }
 
 export default SidebarWithHeader
